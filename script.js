1f99651de80a8c7e6a6af9b7b36b5dc75e40ce11
tyleregan <TylerEgan@u.boisestate.edu>
2017-09-18 19:11:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+	
+ function myFunction(){
+	 var x = document.getElementById('Answers');
+	 if(x.style.display === 'none'){
+		 x.style.display = 'block';
+	 } else {
+		 x.style.display = 'none';
+	 }
+ }
+}
